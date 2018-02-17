985d1745f82fca9f2dfdb8cb6823de3a5dcb267a
mitpjones <mit_jones@yahoo.co.uk>
2018-02-17 15:03:54 +1300

    Removed options

--- app/src/main/java/demo/simple/com/symdssimpledemo/MainActivity.java
+++ app/src/main/java/demo/simple/com/symdssimpledemo/MainActivity.java
@@ -42,17 +42,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         demoDataSource = new DemoDataSource(this.getApplicationContext());
         demoDataSource.open();
@@ -63,28 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         startSymmetricDS();
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
@@ -107,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         final EditText editTextHazardName = (EditText) findViewById(R.id.editText);
         TextWatcher hazardNameTextWatcher = new GenericTextWatcher(editTextHazardName);
         editTextHazardName.addTextChangedListener(hazardNameTextWatcher);
-
 
 
         final CheckBox checkBoxActive = (CheckBox) findViewById(R.id.checkBoxActive);
